7d568311d58bcbbbee37a878630ee30b6cf9c5ef
Gabriel Luiz <93672097+gabrielluizsf@users.noreply.github.com>
2023-04-29 12:29:46 -0300

    Update index.php

--- api/v1/index.php
+++ api/v1/index.php
@@ -3,6 +3,6 @@
 require_once 'core/router.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
-$path = $_SERVER['PATH_INFO'] ?? '/api/v1/login';
+$path = $_SERVER['PATH_INFO'] ?? '/api/v1';
 
 route($method, $path);
